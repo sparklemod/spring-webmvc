b3959fee1cb1d62a19ecd620f6c8e138fd4c66fe
rina <katerina5314@mail.ru>
2025-02-18 08:40:50 +0300

    index

---
+++ src/main/java/com/userinfo/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.userinfo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+	@GetMapping(value = "/")
+	public String printIndex(ModelMap model) {
+		return "index";
+	}
+}
